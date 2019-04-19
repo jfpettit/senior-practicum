d46cc9682f28f756bce46cb9e751831c7917bfe2
Jacob Pettit <jacobpettit@Jacobs-MacBook-Pro.local>
2019-04-18 22:09:05 -0400

    Updates to MC code, moving on to paper writing

--- blackjack_MC/.ipynb_checkpoints/monte_carlo_agent-checkpoint.py
+++ blackjack_MC/.ipynb_checkpoints/monte_carlo_agent-checkpoint.py
@@ -1,10 +1,12 @@
 from collections import defaultdict
+import time
 import numpy as np
 
 class monte_carlo_policy_iteration:
     def __init__(self, num_states, environment, gamma):
         self.num_states = num_states
         self.env = environment()
+        self.blckjck = environment()
         self.gamma = gamma
     
     def find_first_visit(self, state, states):
@@ -66,7 +68,7 @@ class monte_carlo_policy_iteration:
         Q_ = defaultdict(lambda: np.ones(self.env.action_space.n))
         returns = defaultdict(float)
         count = defaultdict(float)
-        
+        start = time.time()
         for ep in range(epochs):
             episode, pi = self.get_control_episode(1000, Q_, epsilon)
             states = [(x[0], x[1]) for x in episode]
@@ -78,41 +80,42 @@ class monte_carlo_policy_iteration:
                 count[state_action] += 1
                 Q_[state][action] = returns[state_action] / count[state_action]
             if ep % 100000 == 0:
+                print('Time elapsed to ', ep, 'iterations: ', time.time() - start)
                 Q_win_rate.append(self.record_win_rate_q_func(Q_, 10000))
                 pi_win_rate.append(self.record_win_rate_policy(pi, 10000))
-            if ep in [0, 10, 100, 1000, 10000, 100000, 100000-1]:
+                np.save('Q_win_rate.npy', np.asarray(Q_win_rate))
+                np.save('pi_win_rate.npy', np.asarray(pi_win_rate))
+            if ep in [0, 10, 100, 1000, 10000, 100000, 1000000, epochs-1]:
                 Q_wr_10.append(self.record_win_rate_q_func(Q_, 10000))
                 pi_wr_10.append(self.record_win_rate_policy(pi, 10000))
+                np.save('Q_wr_10.npy', np.asarray(Q_wr_10))
+                np.save('pi_wr_10.npy', np.asarray(pi_wr_10))
         return Q_, Q_win_rate, Q_wr_10, pi, pi_win_rate, pi_wr_10
     
     def record_win_rate_policy(self, policy, num_games):
-        player_wins, dealer_wins, draw = 0, 0, 0
+        player_wins = 0
         for game in range(num_games):
-            s_ = blckjck._reset()
+            s_ = self.blckjck._reset()
             for i in range(100):
                 pi = policy(s_)
                 action_choice = np.argmax(pi)
-                new_state, reward, over, _ = blckjck._step(action_choice)
+                new_state, reward, over, _ = self.blckjck._step(action_choice)
                 if over:
                     break
                 s_ = new_state
             if reward == 1: player_wins += 1
-            elif reward == -1: dealer_wins += 1
-            else: draw += 1
         return player_wins/num_games
     
     def record_win_rate_q_func(self, q, num_games):
-        player_wins, dealer_wins, draw = 0, 0, 0
+        player_wins = 0
         for game in range(num_games):
-            s_ = blckjck._reset()
+            s_ = self.blckjck._reset()
             for i in range(100):
                 q_s = q[s_]
                 action_choice = np.argmax(q_s)
-                new_state, reward, over, _ = blckjck._step(action_choice)
+                new_state, reward, over, _ = self.blckjck._step(action_choice)
                 if over:
                     break
                 s_ = new_state
             if reward == 1: player_wins += 1
-            elif reward == -1: dealer_wins += 1
-            else: draw += 1
         return player_wins/num_games
